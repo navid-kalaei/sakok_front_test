b5cbe586f99f53056042875a21a8c70bf5c11038
Navid Kalaei <navidkalaei@gmail.com>
2019-05-08 01:38:45 +0430

    feat: implement Tag from representing tags of images

---
+++ src/components/Tag/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
+
+
+const styles = theme => ({
+	chip: {
+		margin: theme.spacing.unit,
+		paddingLeft: theme.spacing.unit,
+	},
+});
+
+function handleDelete() {
+	alert('You clicked the delete icon.'); // eslint-disable-line no-alert
+}
+
+function Tag(props) {
+	const {children, classes} = props
+
+	return (
+		<Chip
+			label={children}
+			onDelete={handleDelete}
+			color="secondary"
+			className={classes.chip}
+		/>
+	)
+}
+
+Tag.propTypes = {
+	children: PropTypes.string.isRequired,
+}
+
+export default withStyles(styles)(Tag)
