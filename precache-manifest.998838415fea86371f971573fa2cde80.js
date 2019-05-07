0a39afa65f96468dd691ad5baf9da233184c6069
Navid Kalaei <navidkalaei@gmail.com>
2019-05-08 01:52:13 +0430

    Updates

---
+++ precache-manifest.998838415fea86371f971573fa2cde80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1017a8263439894a857609f611b9a7c",
+    "url": "/sakok_front_test/index.html"
+  },
+  {
+    "revision": "42a232776ee6d6b56994",
+    "url": "/sakok_front_test/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "a50c85c4a526a32c489e",
+    "url": "/sakok_front_test/static/js/2.69f7ffe4.chunk.js"
+  },
+  {
+    "revision": "42a232776ee6d6b56994",
+    "url": "/sakok_front_test/static/js/main.d0901872.chunk.js"
+  },
+  {
+    "revision": "516e7b2264e02da479fb",
+    "url": "/sakok_front_test/static/js/runtime~main.c2ff365a.js"
+  }
+]);
