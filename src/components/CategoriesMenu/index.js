e9bb5ebf191d348d3c07f696da6e1d8b646c52c0
Navid Kalaei <navidkalaei@gmail.com>
2019-05-10 02:18:05 +0430

    refactor: separate CategoriesMenu style from logic

--- src/components/CategoriesMenu/index.js
+++ src/components/CategoriesMenu/index.js
@@ -3,28 +3,8 @@ import PropTypes from 'prop-types'
 import {withStyles} from '@material-ui/core/styles'
 import MenuItem from '@material-ui/core/MenuItem'
 import TextField from '@material-ui/core/TextField'
+import styles from './styles'
 
-const styles = theme => ({
-	container: {
-		display: 'flex',
-		flexWrap: 'wrap',
-	},
-	textField: {
-		marginLeft: theme.spacing.unit,
-		marginRight: theme.spacing.unit,
-		width: 200,
-		direction: 'rtl',
-	},
-	dense: {
-		marginTop: theme.spacing.unit * 2,
-	},
-	menu: {
-		width: 200,
-	},
-	rtl: {
-		direction: 'rtl',
-	},
-})
 
 const currencies = [
 	{
---
+++ src/components/CategoriesMenu/styles.js
@@ -0,0 +1,23 @@
+const styles = theme => ({
+	container: {
+		display: 'flex',
+		flexWrap: 'wrap',
+	},
+	textField: {
+		marginLeft: theme.spacing.unit,
+		marginRight: theme.spacing.unit,
+		width: 200,
+		direction: 'rtl',
+	},
+	dense: {
+		marginTop: theme.spacing.unit * 2,
+	},
+	menu: {
+		width: 200,
+	},
+	rtl: {
+		direction: 'rtl',
+	},
+})
+
+export default styles
