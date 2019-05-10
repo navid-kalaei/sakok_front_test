2cc17e13e5cc994918f6c79182767ff88460a402
Navid Kalaei <navidkalaei@gmail.com>
2019-05-10 19:49:26 +0430

    feat: render images from backend

--- src/views/Gallery/index.js
+++ src/views/Gallery/index.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import axios from 'axios'
 import {withStyles} from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Card from '../../components/Card'
 import UploadImageDialog from '../../components/UploadImageDialog'
 import tileData from './tileData'
+import BACKEND_API from '../../config/apis'
 
 
 const styles = theme => ({
@@ -15,26 +17,59 @@ const styles = theme => ({
 })
 
 
-function FullWidthGrid(props) {
-	const {isUploadImageDialogOpen, handleUploadImageDialog, classes} = props
+class FullWidthGrid extends Component {
 
-	return (
-		<div className={classes.root}>
-			<Grid container spacing={16}>
-				{tileData.map(image => (
-					<Grid item xs={12} sm={6} md={4} key={image.title}>
-						<Card {...image}/>
-					</Grid>
-				))}
-			</Grid>
+	state = {
+		images: [],
+	}
 
-			<UploadImageDialog
-				handleUploadImageDialog={handleUploadImageDialog}
-				isUploadImageDialogOpen={isUploadImageDialogOpen}
-			/>
+	componentDidMount() {
+		axios.get(BACKEND_API.image).then(({data}) => (this.setState({images: data})))
+	}
 
-		</div>
+
+	addImageToGallery = (id) => (axios.get(`${BACKEND_API.image}/${id}`).then(({data}) => {
+				const newImages = this.state.images
+				newImages.push(data)
+				this.setState({images: newImages})
+			},
+		)
 	)
+
+	render() {
+		const {isUploadImageDialogOpen, handleUploadImageDialog, classes} = this.props
+		const {images} = this.state
+
+		return (
+			<div className={classes.root}>
+				<Grid container spacing={16}>
+					{tileData.map(image => (
+						<Grid item xs={12} sm={6} md={4} key={image.title}>
+							<Card {...image}/>
+						</Grid>
+					))}
+					{images.map(image => (
+						<Grid item xs={12} sm={6} md={4} key={image._id}>
+							<Card
+								src={`data:${image.image.mimetype};base64,${(new Buffer(image.image.data.data)).toString('base64')}`}
+								title={image.title}
+								date={image.createdAt}
+								tags={image.tags}
+								category={image.category.label}
+							/>
+						</Grid>
+					))}
+
+				</Grid>
+
+				<UploadImageDialog
+					handleUploadImageDialog={handleUploadImageDialog}
+					isUploadImageDialogOpen={isUploadImageDialogOpen}
+				/>
+
+			</div>
+		)
+	}
 }
 
 FullWidthGrid.propTypes = {
